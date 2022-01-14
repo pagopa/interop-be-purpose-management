c3f07e434c1423a72606a9d81bedb994889e83e9
galales <25105748+galales@users.noreply.github.com>
2022-01-14 15:57:46 +0100

    pin-942: Init tests

---
+++ src/test/scala/it/pagopa/pdnd/interop/uservice/purposemanagement/SpecHelper.scala
@@ -0,0 +1,64 @@
+package it.pagopa.pdnd.interop.uservice.purposemanagement
+
+import akka.actor
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.model.{HttpMethods, MessageEntity}
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import it.pagopa.pdnd.interop.uservice.purposemanagement.model._
+
+import java.util.UUID
+import scala.concurrent.{ExecutionContext, Future}
+
+trait SpecHelper {
+
+  def createPurpose(purposeId: UUID, seed: PurposeSeed)(implicit
+    ec: ExecutionContext,
+    actorSystem: actor.ActorSystem
+  ): Future[Purpose] =
+    for {
+      data <- Marshal(seed).to[MessageEntity].map(_.dataBytes)
+      _ = (() => mockUUIDSupplier.get).expects().returning(purposeId).once()
+      _ = (() => mockDateTimeSupplier.get).expects().returning(timestamp).once()
+      purpose <- Unmarshal(makeRequest(data, "purposes", HttpMethods.POST)).to[Purpose]
+    } yield purpose
+
+  def createPurposeVersion(purposeId: UUID, versionId: UUID, seed: PurposeVersionSeed)(implicit
+    ec: ExecutionContext,
+    actorSystem: actor.ActorSystem
+  ): Future[PurposeVersion] =
+    for {
+      data <- Marshal(seed).to[MessageEntity].map(_.dataBytes)
+      _ = (() => mockUUIDSupplier.get).expects().returning(versionId).once()
+      _ = (() => mockDateTimeSupplier.get).expects().returning(timestamp).once()
+      purpose <- Unmarshal(makeRequest(data, s"purposes/$purposeId/versions", HttpMethods.POST)).to[PurposeVersion]
+    } yield purpose
+
+//  def getPurpose(id: String)(implicit ec: ExecutionContext, actorSystem: actor.ActorSystem): Future[Purpose] = {
+//
+//    val response = makeRequest(emptyData, s"purpose/$id", HttpMethods.GET)
+//    val result = for {
+//      purpose <- Unmarshal(response).to[Purpose]
+//    } yield purpose
+//    result
+//  }
+//
+//  def activatePurpose(
+//    purpose: Purpose
+//  )(implicit ec: ExecutionContext, actorSystem: actor.ActorSystem): Future[Purpose] = for {
+//    data <- Marshal(StateChangeDetails(changedBy = Some(ChangedBy.CONSUMER)))
+//      .to[MessageEntity]
+//      .map(_.dataBytes)
+//    activated <- Unmarshal(makeRequest(data, s"purposes/${purpose.id.toString}/activate", HttpMethods.POST))
+//      .to[Purpose]
+//  } yield activated
+//
+//  def suspendPurpose(purpose: Purpose)(implicit ec: ExecutionContext, actorSystem: actor.ActorSystem): Future[Purpose] =
+//    for {
+//      data <- Marshal(StateChangeDetails(changedBy = Some(ChangedBy.CONSUMER)))
+//        .to[MessageEntity]
+//        .map(_.dataBytes)
+//      suspended <- Unmarshal(makeRequest(data, s"purposes/${purpose.id.toString}/suspend", HttpMethods.POST))
+//        .to[Purpose]
+//    } yield suspended
+
+}
---
+++ src/test/scala/it/pagopa/pdnd/interop/uservice/purposemanagement/provider/PurposeApiServiceSpec.scala
@@ -0,0 +1,169 @@
+package it.pagopa.pdnd.interop.uservice.purposemanagement.provider
+
+import akka.actor
+import akka.actor.testkit.typed.scaladsl.{ActorTestKit, ScalaTestWithActorTestKit}
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
+import akka.cluster.typed.{Cluster, Join}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.directives.{AuthenticationDirective, SecurityDirectives}
+import it.pagopa.pdnd.interop.commons.utils.AkkaUtils.Authenticator
+import it.pagopa.pdnd.interop.uservice.purposemanagement._
+import it.pagopa.pdnd.interop.uservice.purposemanagement.api.PurposeApi
+import it.pagopa.pdnd.interop.uservice.purposemanagement.api.impl.{PurposeApiMarshallerImpl, PurposeApiServiceImpl}
+import it.pagopa.pdnd.interop.uservice.purposemanagement.model.{
+  Purpose,
+  PurposeSeed,
+  PurposeVersion,
+  PurposeVersionSeed,
+  PurposeVersionState
+}
+import it.pagopa.pdnd.interop.uservice.purposemanagement.server.Controller
+import it.pagopa.pdnd.interop.uservice.purposemanagement.server.impl.Main
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import java.util.UUID
+import scala.concurrent.duration.{Duration, DurationInt}
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
+
+/** Local integration test.
+  *
+  * Starts a local cluster sharding and invokes REST operations on the event sourcing entity
+  */
+class PurposeApiServiceSpec
+    extends ScalaTestWithActorTestKit(SpecConfiguration.config)
+    with AnyWordSpecLike
+    with SpecConfiguration
+    with SpecHelper {
+
+  var controller: Option[Controller]                 = None
+  var bindServer: Option[Future[Http.ServerBinding]] = None
+  val wrappingDirective: AuthenticationDirective[Seq[(String, String)]] =
+    SecurityDirectives.authenticateOAuth2("SecurityRealm", Authenticator)
+
+  val sharding: ClusterSharding = ClusterSharding(system)
+
+  val httpSystem: ActorSystem[Any] =
+    ActorSystem(Behaviors.ignore[Any], name = system.name, config = system.settings.config)
+
+  implicit val executionContext: ExecutionContextExecutor = httpSystem.executionContext
+  implicit val classicSystem: actor.ActorSystem           = httpSystem.classicSystem
+
+  override def beforeAll(): Unit = {
+    val persistentEntity = Main.buildPersistentEntity()
+
+    Cluster(system).manager ! Join(Cluster(system).selfMember.address)
+    sharding.init(persistentEntity)
+
+    val purposeApi = new PurposeApi(
+      new PurposeApiServiceImpl(system, sharding, persistentEntity, mockUUIDSupplier, mockDateTimeSupplier),
+      PurposeApiMarshallerImpl,
+      wrappingDirective
+    )
+
+    controller = Some(new Controller(purposeApi)(classicSystem))
+
+    controller foreach { controller =>
+      bindServer = Some(
+        Http()
+          .newServerAt("0.0.0.0", 18088)
+          .bind(controller.routes)
+      )
+
+      Await.result(bindServer.get, 100.seconds)
+    }
+  }
+
+  override def afterAll(): Unit = {
+    println("****** Cleaning resources ********")
+    bindServer.foreach(_.foreach(_.unbind()))
+    ActorTestKit.shutdown(httpSystem, 5.seconds)
+    super.afterAll()
+    println("Resources cleaned")
+  }
+
+  "Creation of a new purpose" must {
+
+    "succeed" in {
+      val purposeId  = UUID.randomUUID()
+      val eServiceId = UUID.randomUUID()
+      val consumerId = UUID.randomUUID()
+
+      val purposeSeed = PurposeSeed(eserviceId = eServiceId, consumerId = consumerId)
+
+      val response: Future[Purpose] = createPurpose(purposeId, purposeSeed)
+
+      val bodyResponse: Purpose = Await.result(response, Duration.Inf)
+
+      val expected = Purpose(
+        id = purposeId,
+        eserviceId = purposeSeed.eserviceId,
+        consumerId = purposeSeed.consumerId,
+        versions = Seq.empty,
+        suspendedByConsumer = None,
+        suspendedByProducer = None,
+        createdAt = timestamp,
+        updatedAt = None
+      )
+
+      bodyResponse shouldBe expected
+    }
+
+    "Creation of a new purpose version" must {
+
+      "succeed if purpose exists" in {
+        val purposeId  = UUID.randomUUID()
+        val versionId  = UUID.randomUUID()
+        val eServiceId = UUID.randomUUID()
+        val consumerId = UUID.randomUUID()
+
+        val purposeSeed = PurposeSeed(eserviceId = eServiceId, consumerId = consumerId)
+
+        val versionSeed = PurposeVersionSeed(state = PurposeVersionState.ACTIVE)
+
+        val response: Future[PurposeVersion] =
+          for {
+            _      <- createPurpose(purposeId, purposeSeed)
+            result <- createPurposeVersion(purposeId, versionId, versionSeed)
+          } yield result
+
+        val bodyResponse: PurposeVersion = Await.result(response, Duration.Inf)
+
+        val expected =
+          PurposeVersion(id = versionId, state = versionSeed.state, createdAt = timestamp, expectedApprovalDate = None)
+
+        bodyResponse shouldBe expected
+      }
+
+//      "fail if purpose does not exist" in {
+//        val purposeId  = UUID.randomUUID()
+//        val eServiceId = UUID.randomUUID()
+//        val consumerId = UUID.randomUUID()
+//
+//        val purposeSeed = PurposeSeed(eserviceId = eServiceId, consumerId = consumerId)
+//
+//        (() => mockUUIDSupplier.get).expects().returning(purposeId).once()
+//        (() => mockDateTimeSupplier.get).expects().returning(timestamp).once()
+//
+//        val response: Future[Purpose] = createPurpose(purposeSeed)
+//
+//        val bodyResponse: Purpose = Await.result(response, Duration.Inf)
+//
+//        val expected = Purpose(
+//          id = purposeId,
+//          eserviceId = purposeSeed.eserviceId,
+//          consumerId = purposeSeed.consumerId,
+//          versions = Seq.empty,
+//          suspendedByConsumer = None,
+//          suspendedByProducer = None,
+//          createdAt = timestamp,
+//          updatedAt = None
+//        )
+//
+//        bodyResponse shouldBe expected
+//      }
+    }
+  }
+
+}

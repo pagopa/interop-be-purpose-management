14ff1524020ccd6d905c937f9165d7c54332cd16
galales <25105748+galales@users.noreply.github.com>
2022-02-09 19:18:47 +0100

    pin-1045: Config improvements

--- src/main/scala/it/pagopa/pdnd/interop/uservice/purposemanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/purposemanagement/common/system/ApplicationConfiguration.scala
@@ -7,7 +7,7 @@ import scala.jdk.CollectionConverters._
 object ApplicationConfiguration {
   lazy val config: Config = ConfigFactory.load()
 
-  def serverPort: Int          = config.getInt("uservice-purpose-management.port")
-  def jwtAudience: Set[String] = config.getStringList("uservice-purpose-management.jwt.audience").asScala.toSet
+  lazy val serverPort: Int          = config.getInt("purpose-management.port")
+  lazy val jwtAudience: Set[String] = config.getStringList("purpose-management.jwt.audience").asScala.toSet
 
 }

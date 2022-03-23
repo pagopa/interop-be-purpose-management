aeb9fdc7fe72e04109adf1619b239d2ffe76f3ca
Scala Steward <scala-steward@pagopa.it>
2022-03-23 16:46:11 +0000

    Update akka-projection-eventsourced to 1.2.3 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -13,7 +13,7 @@ object Versions {
   lazy val logbackVersion              = "1.2.11"
   lazy val mustacheVersion             = "0.9.10"
   lazy val postgresVersion             = "42.3.3"
-  lazy val projectionVersion           = "1.2.1"
+  lazy val projectionVersion           = "1.2.3"
   lazy val s3Persistence               = "1.0.27"
   lazy val scalaMockVersion            = "5.2.0"
   lazy val scalapbVersion              = "0.11.1"

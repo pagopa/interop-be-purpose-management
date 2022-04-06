61d38f12ffca42fea352cc649074b7b7a481cffd
Scala Steward <scala-steward@pagopa.it>
2022-04-06 05:58:08 +0000

    Update json4s-ext, json4s-jackson to 4.0.5 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   lazy val httpJson4sVersion           = "1.38.2"
   lazy val jacksonVersion              = "2.11.4"
   lazy val jdbcPersistenceVersion      = "5.0.2"
-  lazy val json4sVersion               = "4.0.4"
+  lazy val json4sVersion               = "4.0.5"
   lazy val kamonVersion                = "2.5.1"
   lazy val logbackVersion              = "1.2.11"
   lazy val mustacheVersion             = "0.9.10"

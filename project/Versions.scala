af0613308cb2396693a82f68fdcb33862d01326f
Scala Steward <scala-steward@pagopa.it>
2022-04-16 06:47:02 +0000

    Update postgresql to 42.3.4 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -12,7 +12,7 @@ object Versions {
   lazy val kamonVersion                = "2.5.1"
   lazy val logbackVersion              = "1.2.11"
   lazy val mustacheVersion             = "0.9.10"
-  lazy val postgresVersion             = "42.3.3"
+  lazy val postgresVersion             = "42.3.4"
   lazy val projectionVersion           = "1.2.1"
   lazy val s3Persistence               = "1.0.27"
   lazy val scalaMockVersion            = "5.2.0"

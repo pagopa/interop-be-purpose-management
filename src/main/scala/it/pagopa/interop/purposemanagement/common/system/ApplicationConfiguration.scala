a033dd7f096973df426fbf375d8ea3a05da15af4
Alessio Gallitano <25105748+galales@users.noreply.github.com>
2022-04-28 09:38:43 +0200

    pin-1382: Comma separated strings for accepted audiences (#78)

--- src/main/scala/it/pagopa/interop/purposemanagement/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/interop/purposemanagement/common/system/ApplicationConfiguration.scala
@@ -2,16 +2,15 @@ package it.pagopa.interop.purposemanagement.common.system
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-import scala.jdk.CollectionConverters._
-
 object ApplicationConfiguration {
-  lazy val config: Config = ConfigFactory.load()
+  val config: Config = ConfigFactory.load()
 
-  lazy val serverPort: Int          = config.getInt("purpose-management.port")
-  lazy val jwtAudience: Set[String] = config.getStringList("purpose-management.jwt.audience").asScala.toSet
+  val serverPort: Int          = config.getInt("purpose-management.port")
+  val jwtAudience: Set[String] = config.getString("purpose-management.jwt.audience").split(",").toSet.filter(_.nonEmpty)
 
-  lazy val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
-  def projectionTag(index: Int)        = s"interop-be-purpose-management-persistence|$index"
-  lazy val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
+  val numberOfProjectionTags: Int = config.getInt("akka.cluster.sharding.number-of-shards")
+  def projectionTag(index: Int)   = s"interop-be-purpose-management-persistence|$index"
+  val projectionsEnabled: Boolean = config.getBoolean("akka.projection.enabled")
 
+  require(jwtAudience.nonEmpty, "Audience cannot be empty")
 }

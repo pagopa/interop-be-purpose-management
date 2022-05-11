e1d1e69b7899334616f21154aaa0069518f704a8
galales <25105748+galales@users.noreply.github.com>
2022-05-11 09:53:14 +0200

    pin-1385: Minor code tweaks

--- src/main/scala/it/pagopa/interop/purposemanagement/model/persistence/projection/PurposeCqrsProjection.scala
+++ src/main/scala/it/pagopa/interop/purposemanagement/model/persistence/projection/PurposeCqrsProjection.scala
@@ -1,4 +1,4 @@
-package it.pagopa.interop.purposemanagement.model.persistence
+package it.pagopa.interop.purposemanagement.model.persistence.projection
 
 import akka.Done
 import akka.actor.typed.ActorSystem
@@ -12,12 +12,13 @@ import akka.projection.slick.{SlickHandler, SlickProjection}
 import cats.syntax.all._
 import it.pagopa.interop.purposemanagement.common.system.MongoDbConfig
 import it.pagopa.interop.purposemanagement.model.persistence.JsonFormats._
-import it.pagopa.interop.purposemanagement.model.purpose.PersistentPurpose
+import it.pagopa.interop.purposemanagement.model.persistence._
+import it.pagopa.interop.purposemanagement.model.persistence.projection.models.{CqrsMetadata, SourceEvent}
 import org.mongodb.scala.MongoClient.DEFAULT_CODEC_REGISTRY
 import org.mongodb.scala._
+import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.connection.NettyStreamFactoryFactory
 import org.mongodb.scala.model._
-import org.mongodb.scala.result.UpdateResult
 import org.slf4j.LoggerFactory
 import slick.basic.DatabaseConfig
 import slick.dbio._
@@ -53,50 +54,60 @@ final case class PurposeCqrsProjection(offsetDbConfig: DatabaseConfig[JdbcProfil
   )
 }
 
-// TODO pass db config object for db and collection names
 final case class CqrsProjectionHandler(client: MongoClient, dbName: String, collectionName: String)(implicit
   ec: ExecutionContext
 ) extends SlickHandler[EventEnvelope[Event]] {
 
   private val logger = LoggerFactory.getLogger(getClass)
 
-  // The implementation is not idempotent
+  // Note: the implementation is not idempotent
   override def process(envelope: EventEnvelope[Event]): DBIO[Done] = DBIOAction.from {
-    logger.debug(s"CQRS Projection: writing event with envelop $envelope")
+    logger.trace(s"CQRS Projection: writing event with envelop $envelope")
     val collection: MongoCollection[Document] = client.getDatabase(dbName).getCollection(collectionName)
 
+    val metadata: CqrsMetadata = CqrsMetadata(sourceEvent =
+      SourceEvent(
+        persistenceId = envelope.persistenceId,
+        sequenceNr = envelope.sequenceNr,
+        timestamp = envelope.timestamp
+      )
+    )
+
+    def withMetadata(op: Bson): Bson = Updates.combine(Updates.set("metadata", metadata.toDocument), op)
+
     val result = envelope.event match {
-      case PurposeCreated(p)                  => collection.insertOne(Document(s"{data: ${p.toJson.compactPrint}}"))
+      case PurposeCreated(p)                  =>
+        collection.insertOne(Document(s"{ data: ${p.toJson.compactPrint}, metadata: ${metadata.toJson.compactPrint} }"))
       case PurposeUpdated(p)                  =>
-        collection.updateOne(Filters.eq("data.id", p.id.toString), Updates.set("data", Document(p.toJson.compactPrint)))
+        collection.updateOne(Filters.eq("data.id", p.id.toString), withMetadata(Updates.set("data", p.toDocument)))
       case PurposeVersionCreated(pId, v)      =>
-        collection.updateOne(Filters.eq("data.id", pId), Updates.push("data.versions", Document(v.toJson.compactPrint)))
+        collection.updateOne(Filters.eq("data.id", pId), withMetadata(Updates.push("data.versions", v.toDocument)))
       case PurposeVersionActivated(p)         =>
-        collection.updateOne(Filters.eq("data.id", p.id.toString), Updates.set("data", Document(p.toJson.compactPrint)))
+        collection.updateOne(Filters.eq("data.id", p.id.toString), withMetadata(Updates.set("data", p.toDocument)))
       case PurposeVersionSuspended(p)         =>
-        collection.updateOne(Filters.eq("data.id", p.id.toString), Updates.set("data", Document(p.toJson.compactPrint)))
+        collection.updateOne(Filters.eq("data.id", p.id.toString), withMetadata(Updates.set("data", p.toDocument)))
       case PurposeVersionWaitedForApproval(p) =>
-        collection.updateOne(Filters.eq("data.id", p.id.toString), Updates.set("data", Document(p.toJson.compactPrint)))
+        collection.updateOne(Filters.eq("data.id", p.id.toString), withMetadata(Updates.set("data", p.toDocument)))
       case PurposeVersionArchived(p)          =>
-        collection.updateOne(Filters.eq("data.id", p.id.toString), Updates.set("data", Document(p.toJson.compactPrint)))
+        collection.updateOne(Filters.eq("data.id", p.id.toString), withMetadata(Updates.set("data", p.toDocument)))
       case PurposeVersionUpdated(pId, v)      =>
         collection.updateOne(
           Filters.and(Filters.eq("data.id", pId), Filters.eq("data.versions.id", v.id.toString)),
-          Updates.set("data.versions.$", Document(v.toJson.compactPrint))
+          withMetadata(Updates.set("data.versions.$", v.toDocument))
         )
       case PurposeVersionDeleted(pId, vId)    =>
-        collection.updateOne(Filters.eq("data.id", pId), Updates.pull("data.versions", Document(s"{ id : \"$vId\" }")))
+        collection.updateOne(
+          Filters.eq("data.id", pId),
+          withMetadata(Updates.pull("data.versions", Document(s"{ id : \"$vId\" }")))
+        )
       case PurposeDeleted(pId)                => collection.deleteOne(Filters.eq("data.id", pId))
     }
 
     result.toFuture().as(Done)
   }
 
-  def upsert(collection: MongoCollection[Document], purpose: PersistentPurpose): SingleObservable[UpdateResult] =
-    collection.updateOne(
-      Filters.eq("id", purpose.id.toString),
-      Document(purpose.toJson.compactPrint),
-      UpdateOptions().upsert(true)
-    )
+  implicit class SerializableToDocument[T: JsonWriter](v: T) extends AnyRef {
+    def toDocument = Document(v.toJson.compactPrint)
+  }
 
 }

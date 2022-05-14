44fba5dd584accc68c9e89ed93d0a70f0d542694
Scala Steward <scala-steward@pagopa.it>
2022-05-14 06:59:17 +0000

    Update akka-projection-slick to 1.2.4 in 0.1.x

--- project/Versions.scala
+++ project/Versions.scala
@@ -18,7 +18,7 @@ object Versions {
   lazy val scalaMockVersion            = "5.2.0"
   lazy val scalapbVersion              = "0.11.1"
   lazy val scalatestVersion            = "3.2.11"
-  lazy val slickProjectionVersion      = "1.2.2"
+  lazy val slickProjectionVersion      = "1.2.4"
   lazy val sprayVersion                = "1.3.6"
 }
 

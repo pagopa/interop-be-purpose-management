bfed663ba6396c8e2f2ec1ecd08efc722b8d5ce8
Antonio Gelameris <toniogela89@gmail.com>
2022-12-22 15:33:04 +0100

    Changed logging

--- src/main/scala/it/pagopa/interop/purposemanagement/server/impl/Dependencies.scala
+++ src/main/scala/it/pagopa/interop/purposemanagement/server/impl/Dependencies.scala
@@ -36,8 +36,13 @@ import slick.basic.DatabaseConfig
 import slick.jdbc.JdbcProfile
 
 import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import com.typesafe.scalalogging.Logger
+import it.pagopa.interop.commons.logging.{CanLogContextFields, ContextFieldsToLog}
 
 trait Dependencies {
+
+  implicit val logger: LoggerTakingImplicit[ContextFieldsToLog] =
+    Logger.takingImplicit[ContextFieldsToLog]("OAuth2JWTValidatorAsContexts")
 
   val uuidSupplier: UUIDSupplier               = UUIDSupplier
   val dateTimeSupplier: OffsetDateTimeSupplier = OffsetDateTimeSupplier

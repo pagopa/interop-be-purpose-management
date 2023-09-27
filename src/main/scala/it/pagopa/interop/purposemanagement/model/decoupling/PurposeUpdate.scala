e09c8448fdd586721c4049ff5fa913fb32fff966
AsterITA <fabrizio.musella@gmail.com>
2023-09-27 15:33:05 +0200

    disable update of eservice if purpose in DRAFT (#210)

--- src/main/scala/it/pagopa/interop/purposemanagement/model/decoupling/PurposeUpdate.scala
+++ src/main/scala/it/pagopa/interop/purposemanagement/model/decoupling/PurposeUpdate.scala
@@ -6,14 +6,12 @@ import it.pagopa.interop.purposemanagement.model.persistence.Adapters._
 import it.pagopa.interop.purposemanagement.model.purpose.PersistentRiskAnalysisForm
 
 import java.time.OffsetDateTime
-import java.util.UUID
 
 final case class DraftPurposeVersionUpdate(dailyCalls: Integer, timestamp: OffsetDateTime)
 
 final case class PurposeUpdate(
   title: String,
   description: String,
-  eserviceId: UUID,
   isFreeOfCharge: Boolean,
   freeOfChargeReason: Option[String],
   riskAnalysisForm: Option[PersistentRiskAnalysisForm]
@@ -24,7 +22,6 @@ object PurposeUpdate {
     PurposeUpdate(
       title = payload.title,
       description = payload.description,
-      eserviceId = payload.eserviceId,
       isFreeOfCharge = payload.isFreeOfCharge,
       freeOfChargeReason = payload.freeOfChargeReason,
       riskAnalysisForm = payload.riskAnalysisForm.map(PersistentRiskAnalysisForm.fromSeed(uuidSupplier))
